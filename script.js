cfad751e535fe68fc96b5e5c9a8a54db0fab0937
Alfredo Zuniga <diaz.alfredo2001@gmail.com>
2024-11-30 18:22:11 -0600

    Add UI for human choice selection

--- script.js
+++ script.js
@@ -13,50 +13,86 @@ const getComputerChoice = () => {
 };
 
 
-const getHumanChoice = () => {
-  let choice = prompt("Rock, Paper or Scissors").toLowerCase();
-  while (!["rock", "paper", "scissors"].includes(choice)){
-    console.log("Invalid choice")
-    choice = prompt("Rock, Paper or Scissors").toLowerCase();
+// const getHumanChoice = () => {
+//   let choice = prompt("Rock, Paper or Scissors").toLowerCase();
+//   while (!["rock", "paper", "scissors"].includes(choice)){
+//     console.log("Invalid choice")
+//     choice = prompt("Rock, Paper or Scissors").toLowerCase();
 
-  }
-  return choice.charAt(0).toUpperCase() + choice.slice(1);
-  
-};
+//   }
+//   return choice.charAt(0).toUpperCase() + choice.slice(1);  
+// };
 
 let humanScore = 0
 let computerScore = 0
+const results = document.querySelector(".results");
+let roundNumber = 0;
 
 const playRound = (humanChoice, computerChoice) => {
-    if (humanChoice === computerChoice){
-        console.log("Tie")
-    }
-    else if(humanChoice === "Rock" && computerChoice === "Paper" || humanChoice==="Paper" && computerChoice ==="Scissors" || humanChoice==="Scissors" && computerChoice==="Rock")
-    {
-        console.log("You lose!");
-        computerScore++;
-    }
-    else{
-        console.log("You win!");
-        humanScore++;
-    }
-    console.log(`Choices --> Human: ${humanChoice}, Computer:${computerChoice}`);
-    console.log(`Scores --> Human: ${humanScore}, Computer: ${computerScore}`);
+  let outcome = "";
+  if (humanChoice === computerChoice) {
+    outcome = "Tie";
+} else if (
+    (humanChoice === "Rock" && computerChoice === "Paper") ||
+    (humanChoice === "Paper" && computerChoice === "Scissors") ||
+    (humanChoice === "Scissors" && computerChoice === "Rock")
+) {
+    outcome = "You lose!";
+    computerScore++;
+} else {
+    outcome = "You win!";
+    humanScore++;
 }
-const playGame = () => {
-for (let i=0; i<5; i++){
-    console.log(`\nRound # ${i+1}`)
-    let computer = getComputerChoice();
-    let human = getHumanChoice();
-    playRound(human, computer);
+    roundNumber++;
+    if (roundNumber > 5){
+      let winnerMessage = ""
+      if (humanScore > computerScore){
+        winnerMessage = "HUMAN WON!"
+      }
+      else if(humanScore < computerScore){
+        winnerMessage = "COMPUTER WON!"
+      }
+      else{
+        winnerMessage = "TIEEEE !!!!!!"
+      }
+      results.innerHTML = `<p><strong> ${winnerMessage}</strong> <p>
+          <p>Human ~ ${humanScore}, Computer ~ ${computerScore}</p>`
+      humanScore = 0;
+      computerScore = 0;
+      roundNumber = 0;
+      return;
+    }
+    results.innerHTML = `
+    <p> <strong>Choices</strong>: Human ~ ${humanChoice}, Computer ~ ${computerChoice}</p>
+    <p> <strong>Outcome</strong>: ${outcome} </p>
+    <p> <strong>Round Number</strong>: ${roundNumber}</p>
+    <p> <strong>Scores</strong>: Human ~ ${humanScore}, Computer ~ ${computerScore}</p>
+    `
 
+};
+
+
+//Dom Manipulation
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const handleClick = (e) => {
+  const humanChoice = e.target.textContent || e.target.alt;
+  const computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
 }
-if (humanScore > computerScore){
-    console.log(`Congrats! You won the game`)
-}
-else{
-    console.log(`Sorry! You lost.`)
-}
-console.log()
-}
-playGame();
+rock.addEventListener("click", handleClick)
+paper.addEventListener("click", handleClick)
+scissors.addEventListener("click", handleClick)
+
+const rockImg = document.getElementById("rockImg");
+const paperImg = document.getElementById("paperImg");
+const scissorsImg = document.getElementById("scissorsImg");
+rockImg.addEventListener("click", handleClick)
+paperImg.addEventListener("click", handleClick)
+scissorsImg.addEventListener("click", handleClick)
+
+
+
+
